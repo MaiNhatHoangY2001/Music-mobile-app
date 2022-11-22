90f3466a2b7ec420958607b66a9c13ff1fb2a803
stoky71 <81395239+stoky71@users.noreply.github.com>
2022-11-22 10:33:42 +0700

    fix list songs

--- components/pages/ListMusic/ListMusic.js
+++ components/pages/ListMusic/ListMusic.js
@@ -1,38 +1,12 @@
 import { useContext } from 'react';
-import { Text, View, Image, FlatList, AppRegistry } from 'react-native';
+import { Text, View, Image, FlatList, AppRegistry, TouchableOpacity } from 'react-native';
 import { Link } from 'react-router-native';
 import { MusicContext } from '../../../context/MusicContext';
 import BottomPlaying from '../../Layout';
 import styles from './ListMusic.module.scss';
-
-//import TrackPlayer from 'react-native-track-player'
-
-// const TrackPlayerInit = async () => {
-//     await TrackPlayer.setupPlayer()
-//     await TrackPlayer.add(
-//         {id:6, name:'Birds', singer:'IMAGINE DRAGON', img:require('./image/birds.jpg'), url:"https://res.cloudinary.com/dh0i5v2tb/video/upload/v1668613051/LTDDNC/Imagine_Dragons_Birds_Audio_ft_Elisa_320kbps_fpqjvs.mp3"}
-//     )
-//     return true;
-// }
+import { Ionicons } from '@expo/vector-icons';
 
 function ListMusic() {
-    // const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
-
-    // useEffect(() => {
-    // const startPlayer = async () => {
-    //     let isInit =  await trackPlayerInit();
-    //     setIsTrackPlayerInit(isInit);
-    // }
-    // startPlayer();
-    // }, []);
-
-    // const onButtonPressed = () => {
-    // TrackPlayer.play();
-    // };
-    // const context = useContext(MusicContext);
-    // const num = context.num;
-    // const setNum = context.setNum;
-
     const songs = [
         {
             id: 1,
@@ -76,6 +50,34 @@ function ListMusic() {
             img: require('./image/birds.jpg'),
             url: 'https://res.cloudinary.com/dh0i5v2tb/video/upload/v1668613051/LTDDNC/Imagine_Dragons_Birds_Audio_ft_Elisa_320kbps_fpqjvs.mp3',
         },
+        {
+            id: 7,
+            name: 'Title',
+            singer: 'MEGHAN TRAINOR',
+            img: require('./image/tittle.jpg'),
+            url: 'https://res.cloudinary.com/dh0i5v2tb/video/upload/v1668613051/LTDDNC/Imagine_Dragons_Birds_Audio_ft_Elisa_320kbps_fpqjvs.mp3',
+        },
+        {
+            id: 8,
+            name: 'Sorry',
+            singer: 'JUSTIN BIEBER',
+            img: require('./image/sorry.png'),
+            url: 'https://res.cloudinary.com/dh0i5v2tb/video/upload/v1668613051/LTDDNC/Imagine_Dragons_Birds_Audio_ft_Elisa_320kbps_fpqjvs.mp3',
+        },
+        {
+            id: 9,
+            name: 'How Long',
+            singer: 'CHARLIE PUTH',
+            img: require('./image/howlong.jpg'),
+            url: 'https://res.cloudinary.com/dh0i5v2tb/video/upload/v1668613051/LTDDNC/Imagine_Dragons_Birds_Audio_ft_Elisa_320kbps_fpqjvs.mp3',
+        },
+        {
+            id: 10,
+            name: 'Beautiful Now',
+            singer: 'ZEDD',
+            img: require('./image/beautiful.jpg'),
+            url: 'https://res.cloudinary.com/dh0i5v2tb/video/upload/v1668613051/LTDDNC/Imagine_Dragons_Birds_Audio_ft_Elisa_320kbps_fpqjvs.mp3',
+        },
     ];
     return (
         <View style={styles.container}>
@@ -91,13 +93,12 @@ function ListMusic() {
                 />
             </View>
 
-            <View style={{ flexDirection: 'row', flex: 0.1, marginTop: -70, marginLeft: -220 }}>
-                <Text style={styles.recommend}>Liked Songs</Text>
+            <View style={{ flexDirection: 'row', flex: 0.1, marginTop: -70, marginLeft: -235 }}>
+                <Text style={styles.recommend}>List Songs</Text>
             </View>
 
-            <View style={{ flex: 0.6 }}>
-                <FlatList
-                    numColumns={2}
+            <View style={{ flex: 0.6, width:"100%", alignSelf:'flex-start' }}>
+                <FlatList key={'#'}
                     data={songs}
                     renderItem={({ item, index }) => {
                         return (
@@ -111,9 +112,20 @@ function ListMusic() {
                                     elevation: 5,
                                 }}
                             >
-                                <Image source={item.img} style={{ height: 200, width: 170, borderRadius: 12 }} />
-                                <Text style={styles.nameSong}>{item.name}</Text>
-                                <Text style={styles.nameSinger}>{item.singer}</Text>
+                                <View style={{flexDirection:'row'}}>
+                                    <View style={{flexDirection:'column',flex:0.25}}>
+                                        <Image source={item.img} style={{ height: 55, width: 55, borderRadius: 12 }} />
+                                    </View>
+                                    <View style={{flexDirection:'column', flex:0.99}}>
+                                        <Text style={styles.nameSong}>{item.name}</Text>
+                                        <Text style={styles.nameSinger}>{item.singer}</Text>
+                                    </View>
+                                    <View style={{flexDirection:'column',flex:0.1}}>
+                                        <TouchableOpacity>
+                                            <Ionicons name="ellipsis-vertical" size={30} color="rgba(137, 150, 184, 0.6);" />
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>    
                             </View>
                         );
                     }}
