c2074212b9b5b688011ed7e4a581d7ef13ceb6cd
Madnok <hungviper016@gmail.com>
2022-11-24 14:45:53 +0700

    playing now fixing

--- components/pages/PlayingNow/PlayingNow.js
+++ components/pages/PlayingNow/PlayingNow.js
@@ -30,7 +30,72 @@ export default function PlayingNow() {
 
     return (
         <SafeAreaView style={[{ paddingTop: STATUSBAR_HEIGHT }, styles.container]}>
-            <View style={styles.header}>
+             const [isMute, setIsMute] = useState(true);
+
+const isLargeNumber = (element) => element.id == song.id;
+
+const handleNextSong = () => {
+    const songNow = songs.findIndex(isLargeNumber);
+    const songFind = songs.find((item, index) => index == songNow + 1);
+    songFind === undefined ? setSong(songs[0]) : setSong(songFind);
+};
+const handleBackSong = () => {
+    const songNow = songs.findIndex(isLargeNumber);
+    const songFind = songs.find((item, index) => index == songNow - 1);
+    songFind === undefined ? setSong(songs[songs.length - 1]) : setSong(songFind);
+};
+return (
+    <SafeAreaView style={[{ paddingTop: STATUSBAR_HEIGHT }, styles.container]}>
+        <View style={styles.header}>
+            <Link to="/" style={styles.back}>
+                <Ionicons name="arrow-back" size={24} color="white" />
+            </Link>
+            <View style={styles.bgTitle}>
+                <Text style={styles.title}>Playing Now</Text>
+            </View>
+        </View>
+        <View style={styles.contain}>
+            <View style={styles.contentImage}>
+                <Image style={[{ resizeMode: 'contain' }, styles.image]} source={{ uri: song.uri }} />
+                <Text style={styles.nameSong}>{song?.name}</Text>
+                <Text style={styles.nameSinger}>{song?.singer}</Text>
+            </View>
+            <View style={styles.contentAction}>
+                <View style={styles.listIcon}>
+                    <TouchableOpacity onPress={() => setIsMute(!isMute)}>
+                        <Feather style={styles.icon} name={isMute ? 'volume-1' : 'volume-x'} size={24} />
+                    </TouchableOpacity>
+                    <View style={styles.listIconRight}>
+                        <TouchableOpacity style={styles.iconRight}>
+                            <MaterialIcons style={styles.icon} name="loop" size={24} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.iconRight}>
+                            <Ionicons style={styles.icon} name="share-social" size={24} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={[styles.timeMusic]}>
+                    <View style={[styles.bgTime]}>
+                        <Text style={styles.time}>
+                            {timeMusic?.remainingTime?.mins}:{timeMusic?.remainingTime?.secs}
+                        </Text>
+                        <Text style={styles.time}>
+                            {timeMusic?.durationTime?.mins}:{timeMusic?.durationTime?.secs}
+                        </Text>
+                    </View>
+                    <Slider
+                        style={{ width: '100%', height: 40 }}
+                        value={status}
+                        minimumValue={0}
+                        maximumValue={100}
+                        thumbTintColor="#FFFFFF"
+                        minimumTrackTintColor="#FFFFFF"
+                        maximumTrackTintColor="#000000"
+                        onSlidingComplete={onChangeMusicTime}
+                        onSlidingStart={async () => {
+                            await sound.pauseAsync();
+                            
+            {/* <View style={styles.header}>
                 <Link to="/" style={styles.back}>
                     <Ionicons name="arrow-back" size={24} color="white" />
                 </Link>
@@ -93,7 +158,7 @@ export default function PlayingNow() {
                         </TouchableOpacity>
                     </View>
                 </View>
-            </View>
+            </View> */}
         </SafeAreaView>
     );
 }
